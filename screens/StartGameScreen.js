5d5d62d6baa609f09d9b78caed87eaa6377e5cc8
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-07-18 11:08:40 +0200

    styled input container on the StartGameScreen // different ways for Android and IOS regarding the shadow of the box ❗

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,12 +1,29 @@
-import { TextInput, View } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
 import { PrimaryButton } from "../components/PrimaryButton";
 
 export const StartGameScreen = () => {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    borderRadius: 8,
+    backgroundColor: "#72063c",
+    // Shadow for Android
+    elevation: 4,
+    // Shadow for IOS
+    shadowColor: "black",
+    shadowOffset: {width: 0, height: 2},
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});
