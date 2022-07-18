2b7d6e8888375190ea9f8bef685da186bf5c9e16
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-07-18 11:21:17 +0200

    limited text input to 2 characters + styled the text input component

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,7 +4,7 @@ import { PrimaryButton } from "../components/PrimaryButton";
 export const StartGameScreen = () => {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput style={styles.numberInput} maxLength={2}/>
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -25,5 +25,17 @@ const styles = StyleSheet.create({
     shadowOffset: {width: 0, height: 2},
     shadowRadius: 6,
     shadowOpacity: 0.25,
+  },
+  numberInput: {
+    alignSelf: "center",
+    marginVertical: 8,
+    height: 50,
+    width: 100,
+    textAlign: "center",
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "#ddb52f",
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2, // needed to vizualize the border
   },
 });
