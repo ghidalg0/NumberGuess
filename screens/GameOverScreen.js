91b233a5607145bc70ce6e6e2d79ebbfefcd8b0b
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-07-19 11:32:06 +0200

    improved GOS with previous concepts

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,30 +1,54 @@
-import { Image, StyleSheet, Text, View, Dimensions } from "react-native";
+import { Image, StyleSheet, Text, View, useWindowDimensions, ScrollView } from "react-native";
 import { PrimaryButton } from "../components/ui/PrimaryButton";
 import { Title } from "../components/ui/Title";
 import { Colors } from "../constants/Colors";
 
 
 export const GameOverScreen = ({ roundsNumber, userNumber, onRestart }) => {
+
+  const {width, height} = useWindowDimensions();
+
+  let imageSize = 300;
+
+  if (width < 380) {
+    imageSize = 150;
+  }
+
+  if (height < 400) {
+    imageSize = 80;
+  }
+
+  const imageStyle = {
+    width: imageSize,
+    height: imageSize,
+    borderRadius: imageSize / 2,
+  };
+
   return (
-    <View style={styles.screenContainer}>
-      <Title>Game Over</Title>
-      <View style={styles.imageContainer}>
-        <Image
-          source={require("../assets/images/success.jpg")}
-          style={styles.imageStyle}
-        />
+    <ScrollView style={styles.root}>
+      <View style={styles.screenContainer}>
+        <Title>Game Over</Title>
+        <View style={[styles.imageContainer, imageStyle]}>
+          <Image
+            source={require("../assets/images/success.jpg")}
+            style={styles.image}
+          />
+        </View>
+        <Text style={styles.summaryText}>
+          Your phone needed <Text style={styles.highlightText}>{roundsNumber}</Text> rounds to guess the number <Text style={styles.highlightText}>{userNumber}</Text> 🔥
+        </Text>
+        <PrimaryButton onPress={onRestart}>Start New Game</PrimaryButton>
       </View>
-      <Text style={styles.summaryText}>
-        Your phone needed <Text style={styles.highlightText}>{roundsNumber}</Text> rounds to guess the number <Text style={styles.highlightText}>{userNumber}</Text> 🔥
-      </Text>
-      <PrimaryButton onPress={onRestart}>Start New Game</PrimaryButton>
-    </View>
+    </ScrollView>
   );
 }
 
-const deviceWidth = Dimensions.get("window").width;
+// const deviceWidth = Dimensions.get("window").width;
 
 const styles = StyleSheet.create({
+  root: {
+    flex: 1
+  },
   screenContainer: {
     flex: 1,
     padding: 24,
@@ -32,16 +56,16 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   imageContainer: {
-    width: deviceWidth < 380 ? 150 : 300,
-    height: deviceWidth < 380 ? 150 : 300,
+    // width: deviceWidth < 380 ? 150 : 300,
+    // height: deviceWidth < 380 ? 150 : 300,
     overflow: "hidden",
-    borderRadius: deviceWidth < 380 ? 75 : 150, // need so be half w and h to make it round
+    // borderRadius: deviceWidth < 380 ? 75 : 150, // need so be half w and h to make it round
     borderWidth: 3,
     borderColor: Colors.primary900,
     alignSelf: "center",
     margin: 36,
   },
-  imageStyle: {
+  image: {
     width: "100%",
     height: "100%",
   },
